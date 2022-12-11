0cd5bea8253045ae303c919baead0cd8deeac6f3
Seo, Myoungjin <sih5673@gmail.com>
2022-12-11 16:35:36 +0900

    서명진 과제제출

---
+++ 서명진/src/main/java/gdsc/skhu/jpa/dto/TeamDTO.java
@@ -0,0 +1,26 @@
+package gdsc.skhu.jpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamDTO {
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime lastModifiedDate;
+}
